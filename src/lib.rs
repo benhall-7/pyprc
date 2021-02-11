876bf816c01b64f842851f959747cf9544b988c1
BenHall-7 <Benjaminjahall@gmail.com>
2021-02-11 10:33:57 -0500

    Add constructor for list and struct params)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use prc::{open, save, ParamKind};
+use prc::hash40::Hash40;
+use prc::*;
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 
@@ -8,9 +9,16 @@ struct Param {
     inner: ParamKind,
 }
 
+#[pyclass]
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Hash {
+    inner: Hash40,
+}
+
 #[pymodule]
 fn pyprc(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Param>()?;
+    m.add_class::<Hash>()?;
     Ok(())
 }
 
@@ -34,13 +42,39 @@ impl Param {
         }
     )*
 
+    #[staticmethod]
+    fn list(mut value: Vec<Param>) -> Self {
+        Param {
+            inner: ParamKind::from(ParamList(value
+                .drain(..)
+                .map(|p| p.inner)
+                .collect::<Vec<_>>()
+            ))
+        }
+    }
+
+    #[staticmethod]
+    fn r#struct(mut value: Vec<(Hash, Param)>) -> Self {
+        Param {
+            inner: ParamKind::from(ParamStruct(value
+                .drain(..)
+                .map(|(h, p)| (h.inner, p.inner))
+                .collect::<Vec<_>>()
+            ))
+        }
+    }
+
     fn save(&self, filename: &str) -> PyResult<()> {
         let ps = self
             .inner
             .try_into_ref()
-            .map_err(|_| PyValueError::new_err("Expected Struct-type Param"))?;
+            .map_err(|_| PyValueError::new_err("Only struct-type Params can be saved to a file"))?;
         save(filename, ps)?;
         Ok(())
+    }
+
+    fn clone(&self) -> Self {
+        Param { inner: self.inner.clone() }
     }
 }
 
@@ -56,5 +90,12 @@ make_impl!(
     (i32, i32),
     (u32, u32),
     (float, f32),
-    (str, String)
+    (str, String),
+    (hash, Hash)
 );
+
+impl From<Hash> for ParamKind {
+    fn from(f: Hash) -> Self {
+        ParamKind::from(f.inner)
+    }
+}
